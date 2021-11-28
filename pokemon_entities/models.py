a087712d50bfe9d2ec0f8ad1d1d0ad4ecd78a27d
elf <elorian@yandex.ru>
2021-11-28 12:03:48 +0300

    translate model fields in russion

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -1,17 +1,21 @@
-from django.db import models  # noqa F401
+from django.db import models
 
 
 class Pokemon(models.Model):
-    title = models.CharField(max_length=200)
-    image = models.ImageField(upload_to='pokemons', null=True, blank=True)
-    description = models.TextField(default='добавить описание')
-    title_en = models.CharField(max_length=200, default='-')
-    title_jp = models.CharField(max_length=200, default='-')
+    title = models.CharField('Название', max_length=200)
+    image = models.ImageField('Изображение', upload_to='pokemons', null=True, blank=True)
+    description = models.TextField('Описание', default='добавить описание')
+    title_en = models.CharField('Название англ.', max_length=200, default='-')
+    title_jp = models.CharField('Название яп.', max_length=200, default='-')
     previous_evolution = models.ForeignKey('self', on_delete=models.SET_NULL,
                                            blank=True, null=True,
                                            verbose_name='Из кого эволюционировал',
                                            related_name='next_evolution'
                                            )
+
+    class Meta:
+        verbose_name = 'Покемон'
+        verbose_name_plural = 'Покемоны'
 
     def __str__(self):
         return '{}'.format(self.title)
@@ -20,14 +24,21 @@ class Pokemon(models.Model):
 class PokemonEntity(models.Model):
     pokemon = models.ForeignKey(
         Pokemon, on_delete=models.CASCADE, null=True, blank=True,
-        related_name='pokemonentity',
+        related_name='pokemonentity', verbose_name='Покемон',
     )
-    lat = models.FloatField()
-    lon = models.FloatField()
-    appeared_at = models.DateTimeField()
-    disappeared_at = models.DateTimeField()
-    level = models.IntegerField(default=0)
-    health = models.IntegerField(default=0)
-    strength = models.IntegerField(default=0)
-    defence = models.IntegerField(default=0)
-    stamina = models.IntegerField(default=0)
+    lat = models.FloatField('Широта')
+    lon = models.FloatField('Долгота')
+    appeared_at = models.DateTimeField('Появляется')
+    disappeared_at = models.DateTimeField('Исчезает')
+    level = models.IntegerField('Уровень', default=0)
+    health = models.IntegerField('Здоровье', default=0)
+    strength = models.IntegerField('Сила', default=0)
+    defence = models.IntegerField('Защита', default=0)
+    stamina = models.IntegerField('Выносливость', default=0)
+
+    class Meta:
+        verbose_name = 'Сущность на карте'
+        verbose_name_plural = 'Сущности на карте'
+
+    def __str__(self):
+        return 'Сущность {}'.format(self.pokemon)
